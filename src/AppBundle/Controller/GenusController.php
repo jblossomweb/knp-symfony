8ed1c130b5805fcebfa00120670a8dab9fb689fa
John Blossom <jblossom.web@gmail.com>
2016-05-11 10:06:22 -0400

    completed knp symfony tut #1 (using jade)

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController extends Controller
+{
+
+    /**
+     * @Route("/genus/")
+     */
+    public function showGenus()
+    {
+        return $this->render('genus/main.jade');
+    }
+
+    /**
+     * @Route("/genus/{genusName}")
+     */
+    public function showAction($genusName)
+    {
+        // $notes = [
+        //     ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+        //     ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+        //     ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        // ];
+
+        return $this->render('genus/show.jade', array(
+            'name'  =>  $genusName,
+            // 'notes' => $notes
+        ));
+    }
+
+    /**
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
+     * @Method("GET")
+     */
+    public function getNotesAction()
+    {
+        $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+
+        $data = [
+            'notes' =>  $notes,
+        ];
+
+        $response = new Response(json_encode($data));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+}
